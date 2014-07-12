dda1e1ecd302c2074931b90bbb74eabd9e013bf5
Jose Manuel Gonzalez Ricart <jmanuelgricart@gmail.com>
2014-07-11 23:39:25 -0400

    se agrega configuracion para mysql

--- expresopy/settings.py
+++ expresopy/settings.py
@@ -64,10 +64,20 @@ WSGI_APPLICATION = 'expresopy.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.6/ref/settings/#databases
 
-DATABASES = {
+"""DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}"""
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'expresopy',
+        'USER': 'root',
+        'PASSWORD': '123456',
+        'HOST': 'localhost',   # Or an IP Address that your DB is hosted on
+        'PORT': '3306',
     }
 }
 
